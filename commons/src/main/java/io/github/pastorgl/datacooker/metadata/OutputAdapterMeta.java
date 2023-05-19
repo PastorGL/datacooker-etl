a6877cbc52ff2116cfd7a4cdde4d693b50df3fb0
Alexey Evdokimov <a.evdokimov@russianit.ru>
2023-05-19 18:05:56 +0400

    Release 3.5.0

--- commons/src/main/java/io/github/pastorgl/datacooker/metadata/OutputAdapterMeta.java
+++ commons/src/main/java/io/github/pastorgl/datacooker/metadata/OutputAdapterMeta.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) 2023 Data Cooker Team and Contributors
+ * This project uses New BSD license with do no evil clause. For full text, check the LICENSE file in the root directory.
+ */
 package io.github.pastorgl.datacooker.metadata;
 
 import io.github.pastorgl.datacooker.data.StreamType;
