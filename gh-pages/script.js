340f7ce3ba8ff517d1fb6adfdb00cfc332542614
Alexey Evdokimov <a.evdokimov@russianit.ru>
2023-08-14 18:41:00 +0400

    better locale separation

--- gh-pages/script.js
+++ gh-pages/script.js
@@ -3,12 +3,12 @@ function selectLocale() {
 
     for (let lang in window.navigator.languages) {
         if (lang.startsWith('en')) {
-            location.href = location.href.replace('index.html', 'gh-pages/index-EN.html');
+            location.href = location.href.replace('index.html', 'gh-pages/en/index.html');
         }
         if (lang.startsWith('ru')) {
-            location.href = location.href.replace('index.html', 'gh-pages/index-RU.html');
+            location.href = location.href.replace('index.html', 'gh-pages/ru/index.html');
         }
     }
 
-    location.href = location.href.replace('index.html', 'gh-pages/index-EN.html');
+    location.href = location.href.replace('index.html', 'gh-pages/en/index.html');
 }
