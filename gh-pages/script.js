b8c9c8a3ab19f725120ef987005e2b7bcaed810a
Alexey Evdokimov <a.evdokimov@russianit.ru>
2023-08-21 17:00:12 +0400

    script.js

--- gh-pages/script.js
+++ gh-pages/script.js
@@ -1,14 +1,15 @@
 function selectLocale() {
     const location = window.location;
 
+    let located = location.href.replace('index.html', 'gh-pages/en/index.html');
     for (let lang in window.navigator.languages) {
         if (lang.startsWith('en')) {
-            location.href = location.href.replace('index.html', 'gh-pages/en/index.html');
+            located = location.href.replace('index.html', 'gh-pages/en/index.html');
         }
         if (lang.startsWith('ru')) {
-            location.href = location.href.replace('index.html', 'gh-pages/ru/index.html');
+            located = location.href.replace('index.html', 'gh-pages/ru/index.html');
         }
     }
 
-    location.href = location.href.replace('index.html', 'gh-pages/en/index.html');
+    location.href = located;
 }
